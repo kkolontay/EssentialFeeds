a3e195aedd423f954991e554966dbed6b7ddf4d1
Konstantin Kolontay <kkolontay@gmail.com>
2021-06-16 17:13:46 -0400

    CreateEssential feed framework

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by kkolontay on 6/16/21.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let imageURL: URL
+}
